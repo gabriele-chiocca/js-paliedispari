e79aa317b709b401ea47cb780c89016edcc3746c
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-08 16:12:50 +0100

    Structure with comments on Pari & Dispari

---
+++ 02-Pari&Dispari/script.js
@@ -0,0 +1,28 @@
+/*
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+*/
+
+//L'utente sceglie pari o dispari
+
+const userChoiceParioDispari = prompt('Scegli Pari o Dispari').trim();
+console.log(userChoiceParioDispari);
+
+//L'utente inserisce un numero da 1 a 5
+
+const userChoiceNumber = prompt('Inserisci un numero da 1 a 5').trim();
+console.log(userChoiceNumber);
+
+//Generiamo un numero random da 1 a 5 per il computer con una funzione
+
+// Sommiamo il numero inserito dall'utente ed il numero random
+
+//Stabilire se la somma è pari o dispari
+
+//Dichiarare vincitore
