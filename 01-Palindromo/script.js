a50e59c999c58171a703aef9224d6ac32d5b1774
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-08 15:58:15 +0100

    Function e commenti

--- 01-Palindromo/script.js
+++ 01-Palindromo/script.js
@@ -5,26 +5,33 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 const userChoice = prompt('Inserisci una parola').trim();
-
 console.log(userChoice);
 
-reverseWord(userChoice);
+//Invertiamo la parola
+const reversed = reverseWord(userChoice);
 
+//Verifichiamo se è palindroma
+const palindroma = verifiedPalindroma(userChoice, reversed);
+
+//Risultato
+
+console.log(palindroma ? "E' palindroma" : "Non è palindroma");
+
+
+//Funzione inverti parola
 function reverseWord(text) {
   let wordReversed = '';
 
-  for (let i = 0; i < userChoice.length; i++) {
+  for (let i = 0; i < text.length; i++) {
     const currentLetter = userChoice[i];
     wordReversed = currentLetter + wordReversed;
   }
-  console.log(wordReversed);
 
   return wordReversed;
 }
 
-
 function verifiedPalindroma(userChoice, wordReversed) {
-
-    const
-
+ 
+  let   
+  return isPalindroma;
 }
